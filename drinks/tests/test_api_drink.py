6a30b3a9729d7ff728c43f311767613d560c3389
k-tohid <130159541+k-tohid@users.noreply.github.com>
2023-12-26 10:57:31 +0330

    create new tests for delete and put drink

--- drinks/tests/test_api_drink.py
+++ drinks/tests/test_api_drink.py
@@ -39,3 +39,44 @@ def test_delete_drink_without_authentication(client, signup_user):
 
     assert del_response.status_code == 403
 
+
+@pytest.mark.django_db
+def test_put_drink_without_authentication(client, signup_user):
+    token = signup_user
+    drink_info = {'name': 'test', 'description': 'new test drink', 'price': 1.0, 'is_publishable': True}
+
+    headers = {'AUTHORIZATION': 'Token ' + token}
+    response = client.post(reverse('drink_list'), drink_info, headers=headers)
+
+    drink_info_put = {'name': 'name_test_put', 'description': 'description_test', 'price': 1.0, 'is_publishable': True}
+    del_response = client.put(reverse('drink_detail', kwargs={'drink_uuid': response.data['uuid']}), drink_info_put)
+
+    assert del_response.status_code == 403
+
+
+@pytest.mark.django_db
+def test_delete_drink_with_authentication(client, signup_user):
+    token = signup_user
+    drink_info = {'name': 'test', 'description': 'new test drink', 'price': 1.0, 'is_publishable': True}
+
+    headers = {'AUTHORIZATION': 'Token ' + token}
+    response = client.post(reverse('drink_list'), drink_info, headers=headers)
+
+    del_response = client.delete(reverse('drink_detail', kwargs={'drink_uuid': response.data['uuid']}), headers=headers)
+
+    assert del_response.status_code == 204
+
+
+@pytest.mark.django_db
+def test_put_drink_with_authentication(client, signup_user):
+    token = signup_user
+    drink_info = {'name': 'test', 'description': 'new test drink', 'price': 1.0, 'is_publishable': True}
+
+    headers = {'AUTHORIZATION': 'Token ' + token}
+    response = client.post(reverse('drink_list'), drink_info, headers=headers)
+
+    drink_info_put = {'name': 'name_test_put', 'description': 'description_test', 'price': 1.0, 'is_publishable': True}
+    del_response = client.put(reverse('drink_detail', kwargs={'drink_uuid': response.data['uuid']}), drink_info_put, headers=headers)
+
+    assert del_response.status_code == 200
+
