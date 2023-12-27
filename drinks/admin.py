306c63e13b7dd65db65c9013a87b6b3e9c215242
k-tohid <130159541+k-tohid@users.noreply.github.com>
2023-12-27 14:33:49 +0330

    add serach and filter field for drink in admin panel

--- drinks/admin.py
+++ drinks/admin.py
@@ -3,5 +3,16 @@ from django.contrib import admin
 from .models import Drink
 from .models import DrinkImage
 
-admin.site.register(Drink)
-admin.site.register(DrinkImage)
+
+@admin.register(Drink)
+class DrinkAdmin(admin.ModelAdmin):
+    # exclude = ('uuid',)
+    # fields = (,)
+    list_display = ['name', 'created_by', 'is_publishable']
+    list_filter = ['created_by', 'is_publishable']
+    # list_display_links = []
+    search_fields = ('name',)
+
+@admin.register(DrinkImage)
+class DrinkImageAdmin(admin.ModelAdmin):
+    pass
--- drinks/models.py
+++ drinks/models.py
@@ -23,3 +23,6 @@ class Drink(models.Model):
 class DrinkImage(models.Model):
     drink = models.ForeignKey(Drink, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to='drinks', null=True, blank=True)
+
+    def __str__(self):
+        return self.drink
